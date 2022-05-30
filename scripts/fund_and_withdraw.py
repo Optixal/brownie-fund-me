c5a4374653f78cdc3dda081865b63843886d2b8b
Shawn Pang <optixal@live.com.sg>
2022-05-30 13:50:17 +0800

    fix: wait for transaction to complete

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -9,17 +9,17 @@ def fund():
     entrance_fee = fund_me.getEntranceFee() + 100  # 0.025 eth = $50, eth = $2000
     print(f"Entrance fee: {entrance_fee}")
     print("Funding..")
-    fund_me.fundUSD({"from": account, "value": entrance_fee})
+    return fund_me.fundUSD({"from": account, "value": entrance_fee})
 
 
 def withdraw():
     fund_me = FundMe[-1]
     account = get_account()
     print("Withdrawing..")
-    fund_me.withdraw({"from": account})
+    return fund_me.withdraw({"from": account})
 
 
 def main():
     deploy_fund_me()
     fund()
-    withdraw()
+    withdraw().wait(1)
